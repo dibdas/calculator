8f237282073ba441c51f8b0808752ff84fed9883
dibdas <dibyandu08das@gmail.com>
2021-06-26 01:22:16 +0530

    modified
    added result default

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,12 +4,12 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/no-typos */
 import React from 'react';
-import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 
 const Display = (props) => (<div><h3>{props.result}</h3></div>);
 
 Display.propTypes = {
-  result: PropTypes.string.isRequired,
+  result: propTypes.string.isRequired,
 };
 Display.defaultProps = {
   result: '0',
