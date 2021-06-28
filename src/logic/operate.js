9fff4fc6d4ee8e7c8c16b98716723901d14fe0ea
dibdas <dibyandu08das@gmail.com>
2021-06-28 18:54:36 +0530

    modified
    added operation "+,%,-,x"

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,7 +1,28 @@
 import Big from 'big.js';
 
 const operate = (numberOne, numberTwo, operation) => {
+  let result = 0;
   const x = Big(numberOne);
   const y = Big(numberTwo);
-  
+  switch (operation) {
+    case '+':
+      result = x.plus(y);
+      break;
+    case '-':
+      result = x.minus(y);
+      break;
+    case '÷':
+      result = x.div(y);
+      break;
+    case 'x':
+      result = x.times(y);
+      break;
+    case '%':
+      result = x.mod(y);
+      break;
+    default:
+      break;
+  }
 };
+
+export default operate;
