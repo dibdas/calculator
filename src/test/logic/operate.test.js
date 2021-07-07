344b3f4c4b260955087cd3e1e8650839e566aad5
dibdas <dibyandu08das@gmail.com>
2021-07-07 18:34:03 +0530

    modified

--- src/test/logic/operate.test.js
+++ src/test/logic/operate.test.js
@@ -3,8 +3,8 @@ import operate from '../../logic/operate';
 describe('operate', () => {
   it('it should return the result of two given numbers and an operation ', () => {
     expect(operate('3', '2', '+')).toBe('5');
-    expect(operate('3', '2', '-')).toBe('1');
-    expect(operate('3', '2', '+')).toBe('5');
-    expect(operate('3', '2', '+')).toBe('5');
+    expect(operate('8', '2', '-')).toBe('6');
+    expect(operate('9', '3', 'x')).toBe('27');
+    expect(operate('7', '2', '/')).toBe('3.5');
   });
 });
