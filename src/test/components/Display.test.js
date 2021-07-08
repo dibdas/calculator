4ff898add56daf2b7b41622f8ba90d637682a935
dibdas <dibyandu08das@gmail.com>
2021-07-08 13:25:08 +0530

    modified
    added more test

--- src/test/components/Display.test.js
+++ src/test/components/Display.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
 import Display from '../../components/Display';
 
 describe('ButtonPanel', () => {
@@ -8,5 +9,10 @@ describe('ButtonPanel', () => {
       .create(<Display />)
       .toJSON();
     expect(tree).toMatchSnapshot();
+  });
+  it('renders the outcome given in as prop', () => {
+    const { container } = render(<Display result="40" />);
+    const card = container.querySelector('div');
+    expect(card).toHaveTextContent('40');
   });
 });
